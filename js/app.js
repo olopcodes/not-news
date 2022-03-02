879ae95dabca53b73b12db4e204fb18b041d4296
olopcodes <olopcodes@gmail.com>
2022-03-01 22:08:32 -0500

    added styles to latest-stories section, typography, button

--- js/app.js
+++ js/app.js
@@ -3,11 +3,15 @@ $(document).ready(async () => {
   await showDailyQuote();
   toggleActiveLink();
   toggleMobileMenu();
+
+  // main articles shown
   await getArticle("hero-item-1", "entertainment", 1);
   await getArticle("hero-item-2", "sports", 1);
-
+  // rendering main to the page
   renderMainArticle("hero-item-1");
   renderMainArticle("hero-item-2");
-
+  // show the description when you hover on the main article
   showMainDescription();
+
+  await getArticle("latest-article-1", "technology", 1);
 });
